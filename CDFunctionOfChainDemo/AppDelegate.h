a7d437135d1a2ca59e2ab65a9e27c6c51a336acd
bbigcd <bbigcd@163.com>
2016-10-07 20:59:58 +0800

    Initial Commit

---
+++ CDFunctionOfChainDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CDFunctionOfChainDemo
+//
+//  Created by chendi on 16/10/7.
+//  Copyright © 2016年 chendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CDFunctionOfChainDemo/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  CDFunctionOfChainDemo
+//
+//  Created by chendi on 16/10/7.
+//  Copyright © 2016年 chendi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
